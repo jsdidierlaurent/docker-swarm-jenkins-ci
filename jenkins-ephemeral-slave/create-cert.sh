03c49ae083fe9b4b13129eb2a46f59bdf408bce0
Jean-Sébastien Didierlaurent <jsdidierlaurent@ankama.com>
2017-04-18 18:21:32 +0200

    Update project for adding swarm ephemeral agent

---
+++ jenkins-ephemeral-slave/create-cert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Using TLS connection to connect jenkins into the Docker Swarm
+# Doc : https://docs.docker.com/engine/security/https/#create-a-ca-server-and-client-keys-with-openssl
+# This script create Certificate for Docker Swarm Manager
+
